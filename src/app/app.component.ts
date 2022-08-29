6304faffffa815d050dd5e6750445ad24552810f
Megan Weaver <110687887+Meganweaver87@users.noreply.github.com>
2022-08-29 10:56:42 -0500

    "Update the search feature to include type and orbit type (bonus 2)"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,9 @@ export class AppComponent {
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
 			let name = this.sourceList[i].name.toLowerCase();
-			if (name.indexOf(searchTerm) >= 0) {
+			let type = this.sourceList[i].type.toLowerCase();
+			let orbitType = this.sourceList[i].orbitType.toLowerCase();
+			if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0 || orbitType.indexOf(searchTerm) >= 0) {
 				matchingSatellites.push(this.sourceList[i]);
 			}
 		}
